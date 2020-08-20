4da008b8d5339db4d58272cfb9067495c49738da
Huy Nguyen <69789615+giahuy-nguyen@users.noreply.github.com>
2020-08-20 11:48:45 +0700

    Add files via upload

---
+++ sword.cpp
@@ -0,0 +1,16 @@
+#ifndef _definition_h_
+#include "defs.h"
+#define _definition_h_
+#endif
+
+report*  walkthrough (knight& theKnight, castle arrCastle[], int nCastle, int mode, int nPetal)
+{
+  report* pReturn;
+  int bFlag;
+  //fighting for the existence of mankind here
+  
+  
+  // success or failure?	
+  pReturn = (bFlag)? new report : NULL;
+  return pReturn;
+}
